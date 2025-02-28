fac7391ce66b7a5e14432466c8f9c3ec5b6f29f4
orcas-6 <irouah2@gmail.com>
2025-02-28 19:19:15 +0000

    the end ;(

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    root: "client",
     server: {
         open: true,
-    },
-    build: {
-        outDir: "../dist", // Output build files to work4u_FE/dist/ (outside client/)
-        emptyOutDir: true,
     },
 });
